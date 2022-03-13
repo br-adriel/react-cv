b80611917d5660d27b143be66cb1d6c315e9471d
Adriel <adriel.fariasan@gmail.com>
2022-03-13 19:33:16 -0300

    Adiciona formulario de dados de contato

---
+++ src/components/FormContato.js
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+import FormField from './FormField';
+
+class FormContato extends Component {
+  render() {
+    return (
+      <>
+        <h2>Informações de contato</h2>
+        <form action=''>
+          <FormField
+            id='nome'
+            label='Nome completo:'
+            atributos={{
+              type: 'text',
+              required: true,
+              minlength: 3,
+              name: 'nome',
+            }}
+          />
+          <FormField
+            id='celular'
+            label='Celular:'
+            atributos={{
+              type: 'tel',
+              minlength: 9,
+              maxlength: 11,
+              name: 'celular',
+            }}
+          />
+          <FormField
+            id='email'
+            label='Email:'
+            atributos={{
+              type: 'email',
+              name: 'email',
+            }}
+          />
+          <FormField
+            id='linkedin'
+            label='Linkedin:'
+            atributos={{
+              type: 'text',
+              name: 'linkedin',
+            }}
+          />
+          <button type='submit'>Próximo</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default FormContato;
