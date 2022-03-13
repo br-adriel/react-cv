1c462c8d5f7bc254b71c648736d5f179e7248342
Adriel <adriel.fariasan@gmail.com>
2022-03-12 22:14:58 -0300

    Adiciona Header ao App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
+      <Header />
+      <main></main>
       <Footer />
     </>
   );
