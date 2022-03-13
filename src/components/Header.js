1dbe763a54ee87346b64daf41ddeb53f72669240
Adriel <adriel.fariasan@gmail.com>
2022-03-12 22:10:39 -0300

    Adiciona componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+// import '../styles/Header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>meuCurrículo</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
