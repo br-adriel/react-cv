458fc0dccf6716b670e53b6880bf04dc63f292b2
Adriel <adriel.fariasan@gmail.com>
2022-03-13 19:39:07 -0300

    Adiciona sections de divisao de conteudos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <section className='dados'>
+          <Formulario />
+        </section>
+        <section className='curriculo'></section>
+      </main>
       <Footer />
     </>
   );
