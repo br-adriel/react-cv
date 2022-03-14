a6617ba443e3d5f839afddcc8197cd8110ac5e67
Adriel <adriel.fariasan@gmail.com>
2022-03-14 16:36:41 -0300

    Adiciona componente de exibicao de formacoes

---
+++ src/components/Formacoes.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import Formacao from './Formacao';
+import '../styles/Formacoes.css';
+
+class Formacoes extends Component {
+  render() {
+    const { formacoes, apagarFormacao } = this.props;
+    return (
+      <div className='formacoes'>
+        {formacoes.map((formacao, i) => {
+          return (
+            <Formacao
+              key={formacao.id}
+              formacao={formacao}
+              apagarFormacao={apagarFormacao}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Formacoes;
