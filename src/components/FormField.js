64cdc9c2c538708e2d95d72f960d9638b705cfad
Adriel <adriel.fariasan@gmail.com>
2022-03-13 22:15:41 -0300

    Adiciona value e onchange ao input de FormField

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -3,11 +3,16 @@ import '../styles/FormField.css';
 
 class FormField extends Component {
   render() {
-    const { id, label, atributos } = this.props;
+    const { id, label, atributos, iptValue, iptChange } = this.props;
     return (
       <div className='field'>
         <label htmlFor={id}>{label}</label>
-        <input id={id} {...atributos} />
+        <input
+          id={id}
+          {...atributos}
+          value={iptValue}
+          onChange={(e) => iptChange(e)}
+        />
       </div>
     );
   }
