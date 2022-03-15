bf1f9315c5c54bf90eaab0cf74debac2ce19702d
Adriel <adriel.fariasan@gmail.com>
2022-03-15 19:36:19 -0300

    Adiciona componente Curriculo

---
+++ src/components/curriculo/Curriculo.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+// import '../../styles/curriculo/Curriculo.css';
+
+class Curriculo extends Component {
+  render() {
+    const { states } = this.props;
+    return <div className='curriculo'></div>;
+  }
+}
+
+export default Curriculo;
