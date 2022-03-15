e09cf8a207763702bfacb2573f3545ea3131f66c
Adriel <adriel.fariasan@gmail.com>
2022-03-15 20:27:39 -0300

    Implementa Contato em Curriculo

--- src/components/curriculo/Curriculo.js
+++ src/components/curriculo/Curriculo.js
@@ -1,10 +1,15 @@
 import { Component } from 'react';
 import '../../styles/curriculo/Curriculo.css';
+import Contato from './Contato';
 
 class Curriculo extends Component {
   render() {
     const { states } = this.props;
-    return <div className='curriculo'></div>;
+    return (
+      <div className='curriculo'>
+        <Contato states={states} />
+      </div>
+    );
   }
 }
 
