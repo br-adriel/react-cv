3d8d90af56ac2437c216fde2d31a3b17bb93b7f2
Adriel <adriel.fariasan@gmail.com>
2022-03-16 13:17:30 -0300

    Implementa FormacaoAcademica em Curriculo

--- src/components/curriculo/Curriculo.js
+++ src/components/curriculo/Curriculo.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import '../../styles/curriculo/Curriculo.css';
 import Contato from './Contato';
+import FormacaoAcademica from './FormacaoAcademica';
 
 class Curriculo extends Component {
   render() {
@@ -8,6 +9,7 @@ class Curriculo extends Component {
     return (
       <div className='curriculo'>
         <Contato states={states} />
+        <FormacaoAcademica states={states} />
       </div>
     );
   }
