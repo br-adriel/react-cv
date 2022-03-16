6ad7f6501660c03f183cee7f8e24dcce5d547ae9
Adriel <adriel.fariasan@gmail.com>
2022-03-16 13:59:53 -0300

    Implementa Experiencias em Curriculo

--- src/components/curriculo/Curriculo.js
+++ src/components/curriculo/Curriculo.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import '../../styles/curriculo/Curriculo.css';
 import Contato from './Contato';
+import Experiencias from './Experiencias';
 import FormacaoAcademica from './FormacaoAcademica';
 
 class Curriculo extends Component {
@@ -10,6 +11,7 @@ class Curriculo extends Component {
       <div className='curriculo'>
         <Contato states={states} />
         <FormacaoAcademica states={states} />
+        <Experiencias states={states} />
       </div>
     );
   }
