8977b35fb8f1bc903ea7eca85ca393fb73d0fa23
Adriel <adriel.fariasan@gmail.com>
2022-03-16 16:50:23 -0300

    Adiciona state objetivo

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       celular: '',
       email: '',
       linkedin: '',
+      objetivo: '',
       formAtual: 'contato',
       formacoes: [],
       experiencias: [],
