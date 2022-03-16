cbab77ee6a95dcadc36e6c790af231a500ea4ab4
Adriel <adriel.fariasan@gmail.com>
2022-03-16 17:09:58 -0300

    Adiciona componente Objetivo

---
+++ src/components/curriculo/Objetivo.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+// import '../../styles/curriculo/Objetivo.css';
+
+class Objetivo extends Component {
+  render() {
+    const { objetivo } = this.props.states;
+
+    return (
+      <div className='objetivo'>
+        <h2>
+          <FontAwesomeIcon icon={faBriefcase} />
+          Objetivo
+        </h2>
+        <p>{objetivo}</p>
+      </div>
+    );
+  }
+}
+
+export default Objetivo;
