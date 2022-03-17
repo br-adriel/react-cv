22247a0dc07d8e9e9fbcccbc8300a76a3d5c5c1d
Adriel <adriel.fariasan@gmail.com>
2022-03-16 23:00:58 -0300

    Adiciona states para campos de FormFormacao

--- src/components/CadastroFormacoes.js
+++ src/components/CadastroFormacoes.js
@@ -4,19 +4,64 @@ import FormFormacao from './forms/FormFormacao';
 import Formacoes from './Formacoes';
 
 class CadastroFormacoes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codigo: '',
+      curso: '',
+      instituicao: '',
+      inicio: '',
+      fim: '',
+    };
+
+    this.defCurso = this.defCurso.bind(this);
+    this.defInstituicao = this.defInstituicao.bind(this);
+    this.defInicio = this.defInicio.bind(this);
+    this.defFim = this.defFim.bind(this);
+
+    this.btnAtualizar = this.btnAtualizar.bind(this);
+  }
+
+  defCurso = (e) => this.setState({ curso: e.target.value });
+  defInstituicao = (e) => this.setState({ instituicao: e.target.value });
+  defInicio = (e) => this.setState({ inicio: e.target.value });
+  defFim = (e) => this.setState({ fim: e.target.value });
+
+  btnAtualizar = (codigo) => {
+    const formacao = this.props.states.formacoes.filter(
+      (f) => f.id === codigo
+    )[0];
+    this.setState({
+      codigo: formacao.id,
+      curso: formacao.curso,
+      instituicao: formacao.instituicao,
+      inicio: formacao.inicio,
+      fim: formacao.fim,
+    });
+  };
+
   render() {
     const { mudarForm, funcFormacao, states } = this.props;
+    const defFormacao = {
+      curso: this.defCurso,
+      instituicao: this.defInstituicao,
+      inicio: this.defInicio,
+      fim: this.defFim,
+    };
     return (
       <>
         <Card>
           <FormFormacao
             mudarForm={mudarForm}
             novaFormacao={funcFormacao.nova}
+            states={this.state}
+            defFormacao={defFormacao}
           />
         </Card>
         <Formacoes
           formacoes={states.formacoes}
           apagarFormacao={funcFormacao.apagar}
+          editarFormacao={this.btnAtualizar}
         />
       </>
     );
