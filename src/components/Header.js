8f639700d3973284432675fd9e595d1b7945401d
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:04:34 -0300

    Converte 'Header' para function component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import '../styles/Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>meuCurrículo</h1>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header>
+      <h1>meuCurrículo</h1>
+    </header>
+  );
+};
 
 export default Header;
