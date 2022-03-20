48a00a50b2bbd1972c367e70eabe25939e6e7d1f
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:48:44 -0300

    Converte 'Formacoes' em funcao

--- src/components/Formacoes.js
+++ src/components/Formacoes.js
@@ -1,25 +1,22 @@
-import { Component } from 'react';
 import Formacao from './Formacao';
 import '../styles/Formacoes.css';
 
-class Formacoes extends Component {
-  render() {
-    const { formacoes, apagarFormacao, editarFormacao } = this.props;
-    return (
-      <div className='formacoes cards'>
-        {formacoes.map((formacao) => {
-          return (
-            <Formacao
-              key={formacao.id}
-              formacao={formacao}
-              apagarFormacao={apagarFormacao}
-              editarFormacao={editarFormacao}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Formacoes = (props) => {
+  const { formacoes, apagarFormacao, editarFormacao } = props;
+  return (
+    <div className='formacoes cards'>
+      {formacoes.map((formacao) => {
+        return (
+          <Formacao
+            key={formacao.id}
+            formacao={formacao}
+            apagarFormacao={apagarFormacao}
+            editarFormacao={editarFormacao}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Formacoes;
