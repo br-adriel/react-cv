58c08fd0c0fe526c70470395cef23ff15d798d8b
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:48:11 -0300

    Converte 'Experiencias' em funcao

--- src/components/Experiencias.js
+++ src/components/Experiencias.js
@@ -1,24 +1,21 @@
-import { Component } from 'react';
 import Experiencia from './Experiencia';
 
-class Experiencias extends Component {
-  render() {
-    const { experiencias, apagarExperiencia, editarExperiencia } = this.props;
-    return (
-      <div className='cards'>
-        {experiencias.map((experiencia) => {
-          return (
-            <Experiencia
-              key={experiencia.id}
-              experiencia={experiencia}
-              apagarExperiencia={apagarExperiencia}
-              editarExperiencia={editarExperiencia}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Experiencias = (props) => {
+  const { experiencias, apagarExperiencia, editarExperiencia } = props;
+  return (
+    <div className='cards'>
+      {experiencias.map((experiencia) => {
+        return (
+          <Experiencia
+            key={experiencia.id}
+            experiencia={experiencia}
+            apagarExperiencia={apagarExperiencia}
+            editarExperiencia={editarExperiencia}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Experiencias;
