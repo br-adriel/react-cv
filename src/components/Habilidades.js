75ebe963d84b8ec1cdc2472c879b3f6ca1975a0e
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:51:12 -0300

    Converte 'Habilidades' em funcao

--- src/components/Habilidades.js
+++ src/components/Habilidades.js
@@ -1,24 +1,21 @@
-import { Component } from 'react';
 import Habilidade from './Habilidade';
 
-class Habilidades extends Component {
-  render() {
-    const { habilidades, apagarHabilidade, editarHabilidade } = this.props;
-    return (
-      <div className='cards'>
-        {habilidades.map((habilidade) => {
-          return (
-            <Habilidade
-              key={habilidade.id}
-              habilidade={habilidade}
-              apagarHabilidade={apagarHabilidade}
-              editarHabilidade={editarHabilidade}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Habilidades = (props) => {
+  const { habilidades, apagarHabilidade, editarHabilidade } = props;
+  return (
+    <div className='cards'>
+      {habilidades.map((habilidade) => {
+        return (
+          <Habilidade
+            key={habilidade.id}
+            habilidade={habilidade}
+            apagarHabilidade={apagarHabilidade}
+            editarHabilidade={editarHabilidade}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Habilidades;
