fc95b0eda0c7629fdd5c68313b7d7d51aceb5a22
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:51:03 -0300

    Converte 'Habilidade' em funcao

--- src/components/Habilidade.js
+++ src/components/Habilidade.js
@@ -1,33 +1,30 @@
-import { Component } from 'react';
 import Card from './Card';
 import { faTrashCan, faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class Habilidade extends Component {
-  render() {
-    const { habilidade, apagarHabilidade, editarHabilidade } = this.props;
-    return (
-      <Card>
-        <h3>{habilidade.habilidade}</h3>
-        <p>{habilidade.nivel}</p>
-        <div className='botoes'>
-          <button
-            onClick={() => editarHabilidade(habilidade.id)}
-            className='ml-auto'
-          >
-            <FontAwesomeIcon icon={faPen} alt='Ícone de lápis' title='Editar' />
-          </button>
-          <button onClick={() => apagarHabilidade(habilidade.id)}>
-            <FontAwesomeIcon
-              icon={faTrashCan}
-              alt='Ícone de lixeira'
-              title='Apagar'
-            />
-          </button>
-        </div>
-      </Card>
-    );
-  }
-}
+const Habilidade = (props) => {
+  const { habilidade, apagarHabilidade, editarHabilidade } = props;
+  return (
+    <Card>
+      <h3>{habilidade.habilidade}</h3>
+      <p>{habilidade.nivel}</p>
+      <div className='botoes'>
+        <button
+          onClick={() => editarHabilidade(habilidade.id)}
+          className='ml-auto'
+        >
+          <FontAwesomeIcon icon={faPen} alt='Ícone de lápis' title='Editar' />
+        </button>
+        <button onClick={() => apagarHabilidade(habilidade.id)}>
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            alt='Ícone de lixeira'
+            title='Apagar'
+          />
+        </button>
+      </div>
+    </Card>
+  );
+};
 
 export default Habilidade;
