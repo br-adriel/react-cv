19cccc2053233b3684472bff0ba5205f2277fb53
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:03:46 -0300

    Converte 'Footer' para function component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,37 @@
-import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import '../styles/Footer.css';
 
-class Footer extends Component {
-  render() {
-    const ano = new Date().getFullYear();
-    const iconeGithub = <FontAwesomeIcon icon={faGithub} />;
-    const iconeLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
+const Footer = () => {
+  const ano = new Date().getFullYear();
+  const iconeGithub = <FontAwesomeIcon icon={faGithub} />;
+  const iconeLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
 
-    return (
-      <footer>
-        <p className='text'>Adriel Faria, {ano}</p>
-        <div className='icons'>
-          <a
-            href='https://github.com/br-adriel'
-            target='_blank'
-            rel='noopener noreferrer'
-            title='Github'
-            alt='Github'
-          >
-            {iconeGithub}
-          </a>
-          <a
-            href='https://www.linkedin.com/in/adriel-fsantos/'
-            target='_blank'
-            rel='noopener noreferrer'
-            title='LinkedIn'
-            alt='LinkedIn'
-          >
-            {iconeLinkedin}
-          </a>
-        </div>
-      </footer>
-    );
-  }
-}
+  return (
+    <footer>
+      <p className='text'>Adriel Faria, {ano}</p>
+      <div className='icons'>
+        <a
+          href='https://github.com/br-adriel'
+          target='_blank'
+          rel='noopener noreferrer'
+          title='Github'
+          alt='Github'
+        >
+          {iconeGithub}
+        </a>
+        <a
+          href='https://www.linkedin.com/in/adriel-fsantos/'
+          target='_blank'
+          rel='noopener noreferrer'
+          title='LinkedIn'
+          alt='LinkedIn'
+        >
+          {iconeLinkedin}
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
