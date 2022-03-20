fd8c85f284f77351050fde412813bba0019e855d
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:49:18 -0300

    Converte 'FormField' em funcao

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import '../styles/FormField.css';
 
-class FormField extends Component {
-  render() {
-    const { id, label, atributos, iptValue, iptChange = () => {} } = this.props;
-    return (
-      <div className='field'>
-        <label htmlFor={id}>{label}</label>
-        <input
-          id={id}
-          {...atributos}
-          value={iptValue}
-          onChange={(e) => iptChange(e)}
-        />
-      </div>
-    );
-  }
-}
+const FormField = (props) => {
+  const { id, label, atributos, iptValue, iptChange = () => {} } = props;
+  return (
+    <div className='field'>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        {...atributos}
+        value={iptValue}
+        onChange={(e) => iptChange(e)}
+      />
+    </div>
+  );
+};
 
 export default FormField;
