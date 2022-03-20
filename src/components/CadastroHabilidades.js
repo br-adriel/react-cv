1a94012821a554a8db25cdc476f276cd1fa5832a
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:41:54 -0300

    Converte 'CadastroHabilidades' em funcao

--- src/components/CadastroHabilidades.js
+++ src/components/CadastroHabilidades.js
@@ -1,73 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import FormHabilidade from './forms/FormHabilidade';
 import Habilidades from './Habilidades';
 import Card from './Card';
 
-class CadastroHabilidades extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      codigo: '',
-      habilidade: '',
-      nivel: '',
-      textoBotao: 'Adicionar',
-    };
+const CadastroHabilidades = (props) => {
+  const [codigo, setCodigo] = useState('');
+  const [habilidade, setHabilidade] = useState('');
+  const [nivel, setNivel] = useState('');
+  const [textoBotao, setTextoBotao] = useState('Adicionar');
 
-    this.defHabilidade = this.defHabilidade.bind(this);
-    this.defNivel = this.defNivel.bind(this);
-
-    this.limparForm = this.limparForm.bind(this);
-    this.btnAtualizar = this.btnAtualizar.bind(this);
-  }
-
-  defHabilidade = (e) => this.setState({ habilidade: e.target.value });
-  defNivel = (e) => this.setState({ nivel: e.target.value });
-
-  limparForm = () =>
-    this.setState({
-      codigo: '',
-      habilidade: '',
-      nivel: '',
-      textoBotao: 'Adicionar',
-    });
-
-  btnAtualizar = (codigo) => {
-    const habilidasde = this.props.states.habilidades.filter(
-      (f) => f.id === codigo
-    )[0];
-    this.setState({
-      codigo: habilidasde.id,
-      habilidade: habilidasde.habilidade,
-      nivel: habilidasde.nivel,
-      textoBotao: 'Atualizar',
-    });
+  const componentStates = {
+    codigo,
+    habilidade,
+    nivel,
+    textoBotao,
   };
 
-  render() {
-    const { mudarForm, funcHabilidade, states } = this.props;
-    const defHabilidade = {
-      habilidade: this.defHabilidade,
-      nivel: this.defNivel,
-    };
-    return (
-      <>
-        <Card>
-          <FormHabilidade
-            mudarForm={mudarForm}
-            novaHabilidade={funcHabilidade.nova}
-            states={this.state}
-            defHabilidade={defHabilidade}
-            limparForm={this.limparForm}
-          />
-        </Card>
-        <Habilidades
-          habilidades={states.habilidades}
-          apagarHabilidade={funcHabilidade.apagar}
-          editarHabilidade={this.btnAtualizar}
+  const defHabilidade = {
+    habilidade: (e) => setHabilidade(e.arget.value),
+    nivel: (e) => setNivel(e.target.value),
+  };
+
+  const limparForm = () => {
+    setCodigo('');
+    setHabilidade('');
+    setNivel('');
+    setTextoBotao('Adicionar');
+  };
+
+  const btnAtualizar = (codigo) => {
+    const habilidasde = props.states.habilidades.filter(
+      (f) => f.id === codigo
+    )[0];
+    setCodigo(habilidasde.id);
+    setHabilidade(habilidasde.habilidade);
+    setNivel(habilidasde.nivel);
+    setTextoBotao('Atualizar');
+  };
+
+  const { mudarForm, funcHabilidade, states } = props;
+
+  return (
+    <>
+      <Card>
+        <FormHabilidade
+          mudarForm={mudarForm}
+          novaHabilidade={funcHabilidade.nova}
+          states={componentStates}
+          defHabilidade={defHabilidade}
+          limparForm={limparForm}
         />
-      </>
-    );
-  }
-}
+      </Card>
+      <Habilidades
+        habilidades={states.habilidades}
+        apagarHabilidade={funcHabilidade.apagar}
+        editarHabilidade={btnAtualizar}
+      />
+    </>
+  );
+};
 
 export default CadastroHabilidades;
