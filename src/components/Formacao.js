7328353ff091b8d9b685d622c1d2709c6fbd1112
Adriel <adriel.fariasan@gmail.com>
2022-03-20 20:48:24 -0300

    Converte 'Formacao' em funcao

--- src/components/Formacao.js
+++ src/components/Formacao.js
@@ -1,36 +1,30 @@
-import { Component } from 'react';
 import Card from './Card';
 import { faTrashCan, faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class Formacao extends Component {
-  render() {
-    const { formacao, apagarFormacao, editarFormacao } = this.props;
-    return (
-      <Card>
-        <h3>{formacao.curso}</h3>
-        <p>{formacao.instituicao}</p>
-        <p>
-          {formacao.inicio} - {formacao.fim}
-        </p>
-        <div className='botoes'>
-          <button
-            onClick={() => editarFormacao(formacao.id)}
-            className='ml-auto'
-          >
-            <FontAwesomeIcon icon={faPen} alt='Ícone de lápis' title='Editar' />
-          </button>
-          <button onClick={() => apagarFormacao(formacao.id)}>
-            <FontAwesomeIcon
-              icon={faTrashCan}
-              alt='Ícone de lixeira'
-              title='Apagar'
-            />
-          </button>
-        </div>
-      </Card>
-    );
-  }
-}
+const Formacao = (props) => {
+  const { formacao, apagarFormacao, editarFormacao } = props;
+  return (
+    <Card>
+      <h3>{formacao.curso}</h3>
+      <p>{formacao.instituicao}</p>
+      <p>
+        {formacao.inicio} - {formacao.fim}
+      </p>
+      <div className='botoes'>
+        <button onClick={() => editarFormacao(formacao.id)} className='ml-auto'>
+          <FontAwesomeIcon icon={faPen} alt='Ícone de lápis' title='Editar' />
+        </button>
+        <button onClick={() => apagarFormacao(formacao.id)}>
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            alt='Ícone de lixeira'
+            title='Apagar'
+          />
+        </button>
+      </div>
+    </Card>
+  );
+};
 
 export default Formacao;
