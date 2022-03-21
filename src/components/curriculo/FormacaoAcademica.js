327add22be0f959a6d6c0ece17abb408c749a996
Adriel <adriel.fariasan@gmail.com>
2022-03-20 21:11:12 -0300

    Converte 'FormacaoAcademica' em funcao

--- src/components/curriculo/FormacaoAcademica.js
+++ src/components/curriculo/FormacaoAcademica.js
@@ -1,29 +1,26 @@
-import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 
-class FormacaoAcademica extends Component {
-  render() {
-    const { formacoes } = this.props.states;
-    return (
-      <div className='formacaoAcademica'>
-        <h2>
-          <FontAwesomeIcon icon={faGraduationCap} />
-          Formação acadêmica
-        </h2>
-        {[...formacoes].reverse().map((formacao) => {
-          return (
-            <div className='formacao' key={formacao.id}>
-              <h3>{formacao.curso}</h3>
-              <p>
-                {formacao.instituicao} | {formacao.inicio} - {formacao.fim}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const FormacaoAcademica = (props) => {
+  const { formacoes } = props.states;
+  return (
+    <div className='formacaoAcademica'>
+      <h2>
+        <FontAwesomeIcon icon={faGraduationCap} />
+        Formação acadêmica
+      </h2>
+      {[...formacoes].reverse().map((formacao) => {
+        return (
+          <div className='formacao' key={formacao.id}>
+            <h3>{formacao.curso}</h3>
+            <p>
+              {formacao.instituicao} | {formacao.inicio} - {formacao.fim}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default FormacaoAcademica;
