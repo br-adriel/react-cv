379f846cfd233e00cc1eb14e7905c14ab3b33b24
Adriel <adriel.fariasan@gmail.com>
2022-03-20 21:10:20 -0300

    Converte 'Curriculo' em funcao

--- src/components/curriculo/Curriculo.js
+++ src/components/curriculo/Curriculo.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import '../../styles/curriculo/Curriculo.css';
 import Contato from './Contato';
 import Experiencias from './Experiencias';
@@ -6,23 +5,19 @@ import FormacaoAcademica from './FormacaoAcademica';
 import Habilidades from './Habilidades';
 import Objetivo from './Objetivo';
 
-class Curriculo extends Component {
-  render() {
-    const { states } = this.props;
-    return (
-      <div className='curriculo'>
-        <Contato states={states} />
-        {states.objetivo !== '' ? <Objetivo states={states} /> : null}
-        {states.habilidades.length > 0 ? <Habilidades states={states} /> : null}
-        {states.formacoes.length > 0 ? (
-          <FormacaoAcademica states={states} />
-        ) : null}
-        {states.experiencias.length > 0 ? (
-          <Experiencias states={states} />
-        ) : null}
-      </div>
-    );
-  }
-}
+const Curriculo = (props) => {
+  const { states } = props;
+  return (
+    <div className='curriculo'>
+      <Contato states={states} />
+      {states.objetivo !== '' ? <Objetivo states={states} /> : null}
+      {states.habilidades.length > 0 ? <Habilidades states={states} /> : null}
+      {states.formacoes.length > 0 ? (
+        <FormacaoAcademica states={states} />
+      ) : null}
+      {states.experiencias.length > 0 ? <Experiencias states={states} /> : null}
+    </div>
+  );
+};
 
 export default Curriculo;
