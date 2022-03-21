b24bf012f836b76f33a05f31ba33a672821eff35
Adriel <adriel.fariasan@gmail.com>
2022-03-20 21:10:43 -0300

    Converte 'Experiencias' em funcao

--- src/components/curriculo/Experiencias.js
+++ src/components/curriculo/Experiencias.js
@@ -1,54 +1,52 @@
-import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
-class Experiencias extends Component {
-  render() {
-    const meses = [
-      'Jan',
-      'Fev',
-      'Mar',
-      'Abr',
-      'Mai',
-      'Jun',
-      'Jul',
-      'Ago',
-      'Set',
-      'Out',
-      'Nov',
-      'Dez',
-    ];
-    const { experiencias } = this.props.states;
-    return (
-      <div className='experienciasProfissionais'>
-        <h2>
-          <FontAwesomeIcon icon={faClockRotateLeft} />
-          Experiência profissional
-        </h2>
-        {[...experiencias].reverse().map((experiencia) => {
-          let inicio = meses[experiencia.mesInicio - 1];
-          inicio += ' ' + experiencia.anoInicio;
+const Experiencias = (props) => {
+  const meses = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ];
+  const { experiencias } = props.states;
 
-          let fim = meses[experiencia.mesFim - 1];
-          fim += ' ' + experiencia.anoFim;
+  return (
+    <div className='experienciasProfissionais'>
+      <h2>
+        <FontAwesomeIcon icon={faClockRotateLeft} />
+        Experiência profissional
+      </h2>
+      {[...experiencias].reverse().map((experiencia) => {
+        let inicio = meses[experiencia.mesInicio - 1];
+        inicio += ' ' + experiencia.anoInicio;
 
-          return (
-            <div className='experiencia' key={experiencia.id}>
-              <h3>{experiencia.cargo}</h3>
-              <p>
-                {experiencia.empresa} | {inicio} - {fim}
-              </p>
-              <ul>
-                {experiencia.tarefas.map((tarefa, i) => {
-                  return <li key={i}>{tarefa}</li>;
-                })}
-              </ul>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+        let fim = meses[experiencia.mesFim - 1];
+        fim += ' ' + experiencia.anoFim;
+
+        return (
+          <div className='experiencia' key={experiencia.id}>
+            <h3>{experiencia.cargo}</h3>
+            <p>
+              {experiencia.empresa} | {inicio} - {fim}
+            </p>
+            <ul>
+              {experiencia.tarefas.map((tarefa, i) => {
+                return <li key={i}>{tarefa}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Experiencias;
