a288506601f94de8c059f096756c65e809a380af
Adriel <adriel.fariasan@gmail.com>
2022-03-20 21:12:54 -0300

    Converte 'FormContato' em funcao

--- src/components/forms/FormContato.js
+++ src/components/forms/FormContato.js
@@ -1,70 +1,67 @@
-import { Component } from 'react';
 import FormField from '../FormField';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-class FormContato extends Component {
-  render() {
-    const { defContatos, states, mudarForm } = this.props;
-    return (
-      <>
-        <h2>Informações de contato</h2>
-        <form action='' onSubmit={() => mudarForm('objetivo')}>
-          <FormField
-            id='nome'
-            label='Nome completo:'
-            atributos={{
-              type: 'text',
-              required: true,
-              minLength: 3,
-              name: 'nome',
-            }}
-            iptValue={states.nome}
-            iptChange={defContatos.defNome}
+const FormContato = (props) => {
+  const { defContatos, states, mudarForm } = props;
+  return (
+    <>
+      <h2>Informações de contato</h2>
+      <form action='' onSubmit={() => mudarForm('objetivo')}>
+        <FormField
+          id='nome'
+          label='Nome completo:'
+          atributos={{
+            type: 'text',
+            required: true,
+            minLength: 3,
+            name: 'nome',
+          }}
+          iptValue={states.nome}
+          iptChange={defContatos.defNome}
+        />
+        <FormField
+          id='celular'
+          label='Celular:'
+          atributos={{
+            type: 'tel',
+            minLength: 11,
+            maxLength: 11,
+            name: 'celular',
+          }}
+          iptValue={states.celular}
+          iptChange={defContatos.defCelular}
+        />
+        <FormField
+          id='email'
+          label='Email:'
+          atributos={{
+            type: 'email',
+            name: 'email',
+          }}
+          iptValue={states.email}
+          iptChange={defContatos.defEmail}
+        />
+        <FormField
+          id='linkedin'
+          label='Linkedin:'
+          atributos={{
+            type: 'text',
+            name: 'linkedin',
+          }}
+          iptValue={states.linkedin}
+          iptChange={defContatos.defLinkedin}
+        />
+        <button type='submit'>
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            alt='Seta para a direita'
+            title='Próximo'
           />
-          <FormField
-            id='celular'
-            label='Celular:'
-            atributos={{
-              type: 'tel',
-              minLength: 11,
-              maxLength: 11,
-              name: 'celular',
-            }}
-            iptValue={states.celular}
-            iptChange={defContatos.defCelular}
-          />
-          <FormField
-            id='email'
-            label='Email:'
-            atributos={{
-              type: 'email',
-              name: 'email',
-            }}
-            iptValue={states.email}
-            iptChange={defContatos.defEmail}
-          />
-          <FormField
-            id='linkedin'
-            label='Linkedin:'
-            atributos={{
-              type: 'text',
-              name: 'linkedin',
-            }}
-            iptValue={states.linkedin}
-            iptChange={defContatos.defLinkedin}
-          />
-          <button type='submit'>
-            <FontAwesomeIcon
-              icon={faArrowRight}
-              alt='Seta para a direita'
-              title='Próximo'
-            />
-          </button>
-        </form>
-      </>
-    );
-  }
-}
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default FormContato;
